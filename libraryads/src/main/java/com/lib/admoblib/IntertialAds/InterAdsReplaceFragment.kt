bbeac6fd94c9c17e79f762041cb30d9b94deda76
kashif98A <iqlevel51@gmail.com>
2024-08-11 18:57:43 +0500

    new update

---
+++ libraryads/src/main/java/com/lib/admoblib/IntertialAds/InterAdsReplaceFragment.kt
@@ -0,0 +1,64 @@
+package com.lib.admoblib.IntertialAds
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import com.lib.admoblib.isNetworkConnected
+
+
+object InterAdsReplaceFragment {
+    @SuppressLint("SuspiciousIndentation")
+    fun startLoadAdFagment(
+        context: Context,
+        actionid: String,
+        mBundle: Bundle,
+        idAds: String,
+        status: Boolean
+    ) {
+        if (context.isNetworkConnected()) {
+            when {
+                status -> {
+                    ConstantAds.currentTime = System.currentTimeMillis()
+                    if (ConstantAds.currentTime - ConstantAds.lastClickTime > 8000) {
+                        ConstantAds.lastClickTime = ConstantAds.currentTime
+                        ConstantAds.interstitialid = idAds
+                        val mIntent = Intent(context, LoadAdsFragment::class.java)
+                        mIntent.putExtra("actionid", actionid)
+                        mIntent.putExtra("mBundle", mBundle)
+                        context.startActivity(mIntent)
+                        Log.d("checkSide", "startLoadAdActivity: ads phase")
+                    } else {
+                        try {
+                            nextActivity(actionid!!,0, mBundle)
+                            Log.d("checkSide", "startLoadAdActivity: ads phasing")
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        }
+                    }
+                }
+
+                else -> {
+                    try {
+                        nextActivity(actionid,0, mBundle)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+
+        } else {
+            try {
+                nextActivity(actionid,0, mBundle)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+    private fun nextActivity(nextClassNameInString: String, intValue: Int, strValue: Bundle?) {
+        val myData = MyData2(nextClassNameInString, intValue, strValue!!)
+        LoadAdsReplaceFragment.mutableLiveData!!.postValue(myData)
+
+    }
+}
--- libraryads/src/main/java/com/lib/admoblib/IntertialAds/MyData.kt
+++ libraryads/src/main/java/com/lib/admoblib/IntertialAds/MyData.kt
@@ -3,3 +3,4 @@ package com.lib.admoblib.IntertialAds
 import android.os.Bundle
 
 data class MyData(var nextClassName: Int, var intValue: Int,var mBundel: Bundle)
+data class MyData2(var nextClassName: String, var intValue: Int,var mBundel: Bundle)
